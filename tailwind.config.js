8e2a6e24a77a2953596d1649742d91a30ba2e4a6
Heli Joshi <helijoshi45@gmail.com>
2024-03-06 19:37:38 +0530

    Updated UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+    },
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+      },
+      colors: {
+        white: "#fff",
+        gray: {
+          100: "#fffdf9",
+          200: "rgba(255, 255, 255, 0.7)",
+          300: "rgba(255, 253, 249, 0.7)",
+          400: "rgba(255, 253, 249, 0.3)",
+          500: "rgba(255, 253, 249, 0.6)",
+          600: "rgba(255, 255, 255, 0.3)",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
