09004b5c334ba81f81d2210161053a9557fc74d3
Heli Joshi <helijoshi45@gmail.com>
2024-03-08 11:03:05 +0530

    updated toggle-mode

--- src/script.js
+++ src/script.js
@@ -1,14 +1,5 @@
 function toggleMode() {
-  console.log("Toggle mode");
-  const calculator = document.getElementById("calculator");
-  calculator.classList.toggle("dark");
-  document.getElementById("calculator").style.background = "#000";
-  var color = document.getElementById("calculator").style.backgroundColor;
-  console.log(color);
-  document.getElementById("calculator").style.color = "#fff ";
+  document.documentElement.classList.toggle("dark");
 }
 
-// Event listener for mode toggle button
 document.getElementById("mode-toggle").addEventListener("click", toggleMode);
-document.getElementById("calculator").style.background = "#ffffff";
-document.getElementById("calculator").style.color = "#000";
